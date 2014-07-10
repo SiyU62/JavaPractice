869fb4a1f71ec8187d3fde6e646e31ac17722ed5
SiyU62 <kmn946g@gmail.com>
2014-07-10 23:14:03 +0900

    Program01
    1つ目のwhile文ループ・”owari”抜け。

--- Main.java
+++ Main.java
@@ -4,22 +4,25 @@ import java.io.InputStreamReader;
 import java.io.PrintWriter;
 
 public class Main {
-	// private指定しとくとクラス外からのアクセスを防げる。
-	// 場合によるけどグローバル変数はprivateしとくとバグを防げる。
-	// BufferedReaderで入力を、PrintWriterで出力を管理。
 	private BufferedReader Br;
 	private PrintWriter Pw;
 
-	// コンストラクタ。static回避したいだけ。
 	public Main() {
 		Br = new BufferedReader(new InputStreamReader(System.in));
 		Pw = new PrintWriter(System.out);
+		while (true) {
+			try {
+				String str = Br.readLine();
+				if (str.equalsIgnoreCase("owari")) {
+					break;
+				}
+				Pw.println(str);
+				Pw.flush();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
 		try {
-			// 一行読み込んでそのまま出力してる。
-			String str = Br.readLine();
-			Pw.println(str);
-			Pw.flush();
-			// 使い終わったので閉じとく。
 			Br.close();
 			Pw.close();
 		} catch (IOException e) {
